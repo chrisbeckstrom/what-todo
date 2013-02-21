a9b0ab0b1e233f68b701a58c625c4d6271ed38e8
Chris Beckstrom <chrisbeckstrom@gmail.com>
2013-02-21 11:31:39 -0600

    todo.php

--- todo.php
+++ todo.php
@@ -12,17 +12,7 @@
 NOTE: I'm a totally beginner coder, so forgive my errors...!
 */
 
-// CONFIG
-$DBurl = "DBURL";				// the hostname of your DB i.e. "localhost"
-$DBuser = "USER";				// the DB user
-$DBpass = "PASSWORD";			// that user's password
-$DBdb = "DBNAME";				// the name of your mytinytodo DB
-
-// The file: the script will write the todo items to this file
-// the idea is to make it easy to do other stuff with that information
-// like have conky display to contents of that file
-// NOTE: this file must exist! just do $touch /your/directory/yourfilename.txt
-$file = 'YOURPATH';				// php will spit out the results to this file
+include 'config.php';			// get the settings from the config file
 
 // OPEN UP A FILE
 $fh = fopen($file, 'w') or die("can't open file");
